69fffe5e701bdafb0f572836d48a15efebb6daf8
Vojtech Kral <kral.vojtech@gmail.com>
2010-10-03 01:17:34 +0200

    Padding

--- kumodule.c
+++ kumodule.c
@@ -7,6 +7,7 @@
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/random.h>
+#include <linux/slab.h>
 
 #include <asm/uaccess.h>
 
@@ -16,11 +17,11 @@
 #define ku_numdevices 2
 #define ku_maxfortunes 128
 
-
-#define nl(text) (text"\n")
-#define fortuna(text, url) (text"\n")
-#define padding "                                            "
-#define fortuna_sg(text, author, url) (text"\n"padding"--"author"\n")
+const unsigned int ku_maxwidth = 100;             //width padding
+const unsigned int ku_fortune_maxsize = 1024;     //one-kilobyte-sized fortune should be enough for anybody
+#define fortuna(text, url) (text)
+#define padding "                                                                    "
+#define fortuna_sg(text, author, url) (text"\n"padding"--"author)
 
 #define ku_def_readfunc(num) \
     static ssize_t ku_read_##num(struct file * file, char * buf,\
@@ -52,11 +53,33 @@ static const char *ku_fortunes[ku_numdevices][ku_maxfortunes] =
     fortuna_sg("Tím šťastlivcem se stal soutěžící kotyz, který s odpovědí přispěchal jen několik minut po vyhlášení soutěže a dostane tedy téměř dvoumetrové plátno s leteckým snímkem centra Sydney s rozlišením 8 541 x 4 357 pixelů.",
                "zive.cz",
                "http://www.zive.cz/bleskovky/vysledky-souteze-o-obrovsky-snimek-z-google-earth/sc-4-a-148305/default.aspx"),
+    fortuna_sg("Ty mi děláš torčr!",
+               "vlastikroot",
+               "(MoR)"),
     NULL
   },
   /*** /dev/finger ***/
   {
-    //nl("priklad"),
+    "58% uživatelů desktopových PC používá Linux",
+    "OGG poskytuje kvalitnější zvuk než MP3",
+    "Emacs je na práci efektivnější než VIM",
+    "VIM je na práci efektivnější než Emacs",
+    "KDE je mnohem pohodlnější než Gnome",
+    "Gnome je mnohem pohodlnější než KDE",
+    "Zlaté vodiče v kabelu sluchátek poskytují znatelně věrnější poslech",
+    "90% pravicových politiků jsou zločinci",
+    "90% levicových politiků jsou zločinci",
+    "Varování: pracujete v patogenní zóně",
+    "Bůh prokazatelně existuje",
+    "Bůh prokazatelně neexistuje",
+    "Naprostá většina lidí by se uzdravila i bez zásahu léků.",           //Ponkrác
+    "Mluvím naprostou pravdu",
+    "Všechny moje informace jsou ověřené skupinou nezávislých odborníků",
+    "Malá kovová pyramida umí přes noc nabrousit žiletku",
+    "Vytvořením pirátské kopie uchází autorovi zisk",
+    "Regulace internetu pomůže v boji proti terorismu a zneužívání dětí",
+    "Klima se globálně otepluje",
+    "Klima se globálně neotepluje",
     NULL
   },
 };
@@ -66,32 +89,81 @@ static const char *ku_fortunes[ku_numdevices][ku_maxfortunes] =
 static ssize_t ku_read(struct file * file, char * buf, size_t count,
                        loff_t *ppos, const int fortune_number)
 {
-  unsigned int rand;
-  const char **hello_str;
+  unsigned int rand, buf_pos, str_pos, i, last_cpy, num;
+  const char **fortune;
   int len;
+  char buffer[ku_fortune_maxsize + ku_fortune_maxsize/ku_maxwidth + 1];
 
   if (*ppos < 0) return 0;
 
   get_random_bytes(&rand, sizeof(rand));  // get random number and modulo it
+  //rand = 3;
   if (!ku_fortunes_nums[fortune_number]) return 0;
   rand %= ku_fortunes_nums[fortune_number];
 
-  hello_str = &ku_fortunes[fortune_number][rand];
-  len = strlen(*hello_str);               // Don't include the null byte.
+  fortune = &ku_fortunes[fortune_number][rand];
+  len = strlen(*fortune);                 // Don't include the null byte.
 
-  if (count < len) return -EINVAL;        // Only read the whole fortune at once.
-  //printk("count: %d\n", count);
+  if (!len) return 0;
+  if (len > ku_fortune_maxsize) return -EINVAL;
+  if (len > count) return -EINVAL;        // Only read the whole fortune at once.
 
   if (*ppos != 0)	return 0;               // Assume the string has been read
                                           // and indicate there is no more data to be read.
 
-  if (copy_to_user(buf, *hello_str, len))
-    return -EINVAL;
-  len++;
+  //padding
+  for(buf_pos = str_pos = last_cpy = 0; str_pos < len; str_pos++)
+  {
+    num = str_pos-last_cpy+1;
+    //buffer[buf_pos++] = (*fortune)[str_pos];
+    if ((*fortune)[str_pos] == '\n')
+    {
+      //kopirovat od last_cpy po soucasny
+      strncpy(buffer+buf_pos, (*fortune)+last_cpy, num);
+      buf_pos += num;
+      last_cpy = str_pos+1;
+    } else if (num == ku_maxwidth)
+    {
+      //najit mezeru a zkopirovat po mezeru
+      for(i = 0; i < ku_maxwidth; i++)
+        if ((*fortune)[str_pos-i] == ' ')
+        {
+          //zkopirovat po str_pos-i a nahradit mezeru za \n
+          str_pos -= i;
+          num = str_pos-last_cpy+1;
+          strncpy(buffer+buf_pos, (*fortune)+last_cpy, num);
+          buf_pos += num;
+          buffer[buf_pos-1] = '\n';
+          last_cpy = str_pos+1;
+          i = 0;
+          break;
+        }
+      //kdyz se nenajde mezera (tzn. i != 0), zkopirovat po soucasnej a pridat \n
+      // -pozn.: tahle cast poradně nefunguje (utf-8,...). Kaslu na to. Stejne to je extremni situace.
+      // Ale nicemu to nevadi. Prinejhorsim ten text bude vypadat hnusne no, se nikdo snad nezblazni...
+//      if (i)
+//      {
+//        //printk("num: %d\n", num);
+//        strncpy(buffer+buf_pos, (*fortune)+last_cpy, --num);
+//        buf_pos += num;
+//        buffer[buf_pos++] = '\n';
+//        last_cpy = str_pos+1;
+//      }
+    }
+  } //zbyvajici
+  if ((num = str_pos-last_cpy))
+  {
+    strncpy(buffer+buf_pos, (*fortune)+last_cpy, num);
+    buf_pos += num;
+  }
+  buffer[buf_pos++] = '\n';
 
-  *ppos = len;                            // Tell the user how much data we wrote.
-	return len;
+  if (copy_to_user(buf, buffer, buf_pos)) return -EINVAL;
+
+  *ppos = buf_pos;                            // Tell the user how much data we wrote.
+  return buf_pos;
 }
+
 
 /* Read redirect functions */
 
@@ -118,7 +190,7 @@ static int __init ku_init(void)
 {
   int i, j, ret;
 
-  printk(KERN_INFO "Beware: KERNEL ULTRAS kernel module summoned\n");
+  printk(KERN_INFO "KERNEL ULTRAS kernel module summoned\n");
 
   for (i = 0; i < ku_numdevices; i++)
   {
